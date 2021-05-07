54c1b4deefa1faefa067e9a95cf2ce04527c2a6a
Gabriel Fardoski <43648010+GabrielGPF@users.noreply.github.com>
2021-05-07 20:33:35 -0300

    created header button example

--- app/src/main/java/com/gjjg/camera_app_java/AlbumActivity.java
+++ app/src/main/java/com/gjjg/camera_app_java/AlbumActivity.java
@@ -3,6 +3,9 @@ package com.gjjg.camera_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AlbumActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class AlbumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.appbar_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_add:
+                Log.i("test", "CLICOU");
+                break;
+            default:
+                Log.i("test", "ue");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
